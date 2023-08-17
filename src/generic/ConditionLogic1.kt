8c352f2d5004c28757a7931b0f15689bc50c97c4
Anand Lilhare <anandlilhare69@gmail.com>
2023-08-17 12:45:19 +0530

    added new topic in kotlin

---
+++ src/generic/ConditionLogic1.kt
@@ -0,0 +1,11 @@
+package generic
+
+
+fun main() {
+     val valueCount= "hhhPAT"
+    if((!true || !false) && (valueCount == "PAT" || valueCount =="WhiteLable_PAT")) {
+        print("It is release branch")
+    }else {
+        print("It is not release branch")
+    }
+}
